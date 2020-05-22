59433191d51073597a05c22a40ee96ecc07a26cf
bibekebib <42284727+bibekebib@users.noreply.github.com>
2020-05-22 08:33:32 +0545

    bete release

--- typing.js
+++ typing.js
@@ -5,12 +5,14 @@ var speed = document.querySelector('.speed')
 var highest = document.querySelector('.prehigh')
 var words = document.querySelector('.totalwords')
 var finall = document.querySelector('.final')
+var mBody = document.querySelector('.mainBody')
+var pStrings = document.querySelector('.lang').innerHTML
+console.log(pStrings)
+// var pString = typeareas.placeholder
 var totalsec = 0
 var interval
 var timerRun = false
 let time = [0, 0, 0, 0]
-// the user will enter everything here
-// prehigh.style.color = 'red'
 
 // leading zero function
 
@@ -30,12 +32,31 @@ function count() {
     ':' +
     leadingzero(time[2])
   time[3]++
-  speed.innerHTML = 'you can type :' + currentTime
+  speed.innerHTML = 'Total Time Taken :' + currentTime
   time[0] = Math.floor(time[3] / 100 / 60)
   time[1] = Math.floor(time[3] / 100 - time[0] * 60)
   time[2] = Math.floor(time[3] - time[1] * 100 - time[0] * 60000)
 
   return time[3]
+}
+
+function HamroKam() {
+  var textentered = typeareas.value
+  let pString = pStrings.substring(0, textentered.length)
+
+  if (textentered == pString) {
+    clearInterval(interval)
+
+    typeareas.style.borderColor = 'green'
+    finall.innerHTML =
+      'you can write in the speed of :' + final() + ' word per minute'
+  } else {
+    if (textentered == pString) {
+      typeareas.style.borderColor = 'aqua'
+    } else {
+      typeareas.style.borderColor = 'red'
+    }
+  }
 }
 
 function start() {
@@ -45,20 +66,21 @@ function start() {
     timerRun = true
     interval = setInterval(count, 10)
   }
+
   var totta = textentered
-  console.log("total "+totta);
-  // words.innerHTML = 'Total words typed:' + textentered
+  console.log('total ' + totta)
+  words.innerHTML = 'Total words typed:' + textentered
   return totta
 }
 
 function final() {
   clearInterval(interval)
   var sabda = start()
-  var speedo = count()/100
-  console.log(sabda+" "+speedo)
+  var speedo = count() / 60
+  console.log(sabda + ' ' + speedo)
   var actual = Math.floor(sabda / speedo)
   console.log(actual)
-  finall.innerHTML = 'you can write in the speed of :' + actual +" word per second"
+  return actual
 }
 // reset everything
 function TotalReset() {
@@ -67,14 +89,17 @@ function TotalReset() {
   time = [0, 0, 0, 0]
   timerRun = false
   typeareas.value = ''
+  words.innerHTML = 'Total Time Taken :'
+  speed.innerHTML = 'Total Time Taken :'
+  finall.innerHTML = ''
+  // let lolVariableName = final()
+  // highest.innerHTML = 'The previous highest speed was:' + lolVariableName
 }
+
+// function previousHighest() {
+//   var preHighest = final() + preHighest
+// }
 // event will be listened
 
-typeareas.addEventListener("keypress", start, false)
-// submit.addEventListener(onclick, final, false)
-// resetbtn.addEventListener(onclick, TotalReset, false)
-
-// record highest previous
-
-// aba final result
-// console.log(start + ' / ' + count)
+typeareas.addEventListener('keypress', start, false)
+typeareas.addEventListener('keypress', HamroKam, false)
